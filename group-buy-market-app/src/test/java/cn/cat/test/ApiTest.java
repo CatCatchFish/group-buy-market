91425f84589e698f7b4e742c04ce15c740693aad
CatFish <3285109052@qq.com>
2025-04-06 22:23:50 +0800

    引入RabbitMQ消费消息

--- group-buy-market-app/src/test/java/cn/cat/test/ApiTest.java
+++ group-buy-market-app/src/test/java/cn/cat/test/ApiTest.java
@@ -1,9 +1,11 @@
 package cn.cat.test;
 
+import cn.cat.infrastructure.event.EventPublisher;
 import cn.cat.types.util.FutureUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,6 +17,25 @@ import java.util.function.Function;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApiTest {
+
+    @Resource
+    private EventPublisher publisher;
+    @Value("${spring.rabbitmq.config.producer.topic_team_success.routing_key}")
+    private String routingKey;
+
+    @Test
+    public void test_rabbitmq() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        publisher.publish(routingKey, "订单结算：ORD-20231234");
+        publisher.publish(routingKey, "订单结算：ORD-20231235");
+        publisher.publish(routingKey, "订单结算：ORD-20231236");
+        publisher.publish(routingKey, "订单结算：ORD-20231237");
+        publisher.publish(routingKey, "订单结算：ORD-20231238");
+
+        // 等待，消息消费。测试后，可主动关闭。
+        countDownLatch.await();
+    }
 
     @Resource
     private ThreadPoolExecutor threadPoolExecutor;
