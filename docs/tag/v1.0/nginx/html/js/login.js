236bc309646882ff9f5a786d184afe0d345214e1
CatFish <3285109052@qq.com>
2025-04-27 14:13:42 +0800

    微信登录 - v2.0

--- docs/tag/v1.0/nginx/html/js/login.js
+++ docs/tag/v1.0/nginx/html/js/login.js
@@ -5,31 +5,39 @@ document.addEventListener("DOMContentLoaded", () => {
   loginForm.addEventListener("submit", (e) => {
     e.preventDefault()
 
-    let baseUrl = "http://localhost:8091";
-    const username = document.getElementById("username").value
-    const password = document.getElementById("password").value
+    let baseUrl = "http://localhost:8092";
+    const code = document.getElementById("code").value
 
-    fetch(baseUrl + '/api/v1/gbm/tag/add_to_tag', {
+    fetch(baseUrl + '/api/v1/auth/login', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: JSON.stringify({
-        userId: username,
-        tagId: 'CROWD_01'
+      body: new URLSearchParams({
+        code: code  // 替换为实际的验证码
       })
     }).then(response => response.json())
+      .then(data => {
+        if (data.code === '0000') {
+          // 登录成功
+          errorMessage.style.display = "none"
 
-    // 登录成功
-    errorMessage.style.display = "none"
+          // 设置Cookie (有效期为1天)
+          const expirationDate = new Date()
+          expirationDate.setDate(expirationDate.getDate() + 1)
+          document.cookie = `username=${data.data}; expires=${expirationDate.toUTCString()}; path=/`
 
-    // 设置Cookie (有效期为1天)
-    const expirationDate = new Date()
-    expirationDate.setDate(expirationDate.getDate() + 1)
-    document.cookie = `username=${username}; expires=${expirationDate.toUTCString()}; path=/`
-
-    // 跳转到首页
-    window.location.href = "index.html"
+          // 跳转到首页
+          window.location.href = "index.html"
+        } else {
+          console.error('Error:', data.info)
+          alert('登录失败，请检查验证码')
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error)
+        alert('登录失败，请检查验证码')
+      });
   })
 })
 
